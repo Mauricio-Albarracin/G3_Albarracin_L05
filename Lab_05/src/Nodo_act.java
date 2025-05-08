fd1522d9be15d9801c53a4738b9d328603dbd751
unknown <mauricioalbarracinpaye@gmail.com>
2025-05-08 11:06:12 -0500

    Implementación de Nodo básico genérico

---
+++ Lab_05/src/Nodo_act.java
@@ -0,0 +1,9 @@
+public class Nodo_act<T> {
+    T dat; //Dato que se va a almacenar en el nodo
+    Nodo_act<T> siguiente; //Referencia al siguiente nodo
+
+    public Nodo_act(T dat){
+        this.dat = dat;
+        this.siguiente = null;
+    }
+}
